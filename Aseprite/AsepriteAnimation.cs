fc2ca26dd94fc715f23ee1c895a199d9d6bd4ea7
rana-sandouka <60510902+rana-sandouka@users.noreply.github.com>
2023-11-12 17:38:30 +0300

    Add files via upload

---
+++ Aseprite/AsepriteAnimation.cs
@@ -0,0 +1,8 @@
+﻿namespace Aseprite {
+	public class AsepriteAnimation {
+		public int FirstFrame;
+		public int LastFrame;
+		public string Name;
+		public AsepriteTag.LoopDirections Directions;
+	}
+}
